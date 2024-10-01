ed6cc11885345f6f642828e8f4f46b459d284049
mitropoulosg <162826956+mitropoulosg@users.noreply.github.com>
2024-10-01 20:15:24 +0300

    Delete springboot-moneytransfer directory

--- springboot-moneytransfer/src/main/java/com/example/project/dto/TransferResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.dto;
-
-import java.math.BigDecimal;
-import java.util.UUID;
-
-public record TransferResponse(
-        String status,
-        BigDecimal amount
-) {}
--- springboot-moneytransfer/src/main/java/com/example/project/exception/CustomNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.exception;
-
-public class CustomNotFoundException extends RuntimeException {
-
-    public CustomNotFoundException(String message) {
-        super(message);
-    }
-}
